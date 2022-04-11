35c3405968742b9dafecf1b22151296bccf6a105
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 11:00:40 -0700

    RM6785: rootdir: Load WiFi firmware from ODM
    Change-Id: Id18a7aea8d38a62bb871f588d3620583a0e4e562
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc \
     fstab.mt6785 \
-    perf_profile.sh
+    perf_profile.sh \
+    ueventd.oplus.rc
 
 # Keymaster
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -47,3 +47,12 @@ LOCAL_MODULE := perf_profile.sh
 LOCAL_SRC_FILES := bin/perf_profile.sh
 LOCAL_MODULE_CLASS := EXECUTABLES
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.oplus.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.oplus.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM_ETC)
+include $(BUILD_PREBUILT)
